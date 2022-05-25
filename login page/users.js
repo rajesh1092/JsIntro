ff4c067be415aacd43e46b5383077ed28b964d4a
raj <rajesh.kanara@exhibytesolution.com>
2022-05-25 19:29:25 +0530

    users.js

--- login page/users.js
+++ login page/users.js
@@ -16,8 +16,16 @@ const users = async () => {
   var user_Data = JSON.parse(userlist);
   var user_Obj = user_Data["data"];
   console.log(user_Obj);
-  return user_Obj
- // document.getElementById("one").innerHTML= JSON.stringify(user_Obj[1]);
+  var list = $('#list'), container;
+  for(var key in user_Obj){
+    container = $('<div id="user_Obj" class="container"></div>');
+    list.append(container);
+    container.append('<div class="item">' + key +'</div>');
+    container.append('<div class="id">' + grocery_list[key].id +'</div>');
+    container.append('<div class="email">' + grocery_list[key].email +'</div>');
+    container.append('<div class="first_name">' + grocery_list[key].first_name +'</div>');
+    container.append('<div class="last_name">' + grocery_list[key].last_name +'</div>');
+}  
+  //return user_Obj
 };
 users();
-//document.getElementById("one").innerHTML= user_Obj[1];
